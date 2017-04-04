40f8bc254de548a13b92faacb2b12f67084e65f6
东北猫 <song100e@163.com>
2017-04-04 12:16:10 +0800

    视图变量
    视图变量传入与解析

--- app/ctrl/indexCtrl.php
+++ app/ctrl/indexCtrl.php
@@ -1,11 +1,14 @@
 <?php
 namespace app\ctrl;
-class indexCtrl {
+class indexCtrl extends \core\qingsong {
 	public function index(){
-		//p('it is index');
+		$data  = 'Hello QingSong';
 		$model = new \core\lib\model;
 		$sql = 'select id,name from test';
 		$res = $model->query($sql);
-		p($res->fetchAll());
+		//p($res->fetchAll());
+		
+		$this->assign('data', $data);
+		$this->display('index.html');
 	}
 }
